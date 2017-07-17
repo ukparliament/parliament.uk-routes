4ba12a47e07093173c81d5921454bee8faab5c4d
Bryony Watson <watsonb@parliament.uk>
2017-07-17 14:11:35 +0100

    removed dummy files, added to gitignore

--- lib/parliament/engine.rb
+++ lib/parliament/engine.rb
@@ -1,5 +1,19 @@
 module Parliament
   module Engine
-    require 'parliament/engine/engine'
+    def load!
+  		register_rails_files if rails?
+    end
+
+    private
+
+    def rails?
+      defined?(::Rails)
+    end
+
+    def register_rails_files
+      require 'parliament/engine/engine'
+		end
   end
 end
+
+Parliament::Engine.load!
