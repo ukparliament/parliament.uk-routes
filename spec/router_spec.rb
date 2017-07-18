e9d170ea34eb233aad4da7fda0b093d65a14c99f
Bryony Watson <watsonb@parliament.uk>
2017-07-18 10:42:29 +0100

    updated version number and added search route

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'routing' do
 
 it 'should have a specific number of routes inherited from the engine' do
-  # routes.rb currently has 111 routes, 2 routes exist on top of this in dummy application
-  expect(Rails.application.routes.routes.size).to eq(113)
+  # routes.rb currently has 112 routes, 2 routes exist on top of this in dummy application
+  expect(Rails.application.routes.routes.size).to eq(114)
 end
 
 
