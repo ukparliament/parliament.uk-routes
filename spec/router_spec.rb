6356d9935778ed555d57a1412f3ecb608a38414d
Matt Rayner <mattrayner@users.noreply.github.com>
2017-08-25 21:20:50 +0100

    [WEBSITE-1792] Added routes for committees
    * Added routes
    * Updated specs

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'routing' do
-
-it 'should have a specific number of routes inherited from the engine' do
-  # routes.rb currently has 112 routes, 2 routes exist on top of this in dummy application
-  expect(Rails.application.routes.routes.size).to eq(114)
-end
-
-
-
+  it 'should have a specific number of routes inherited from the engine' do
+    # routes.rb currently has 148 routes, 2 routes exist on top of this in dummy application
+    expect(Rails.application.routes.routes.size).to eq(150)
+  end
 end
