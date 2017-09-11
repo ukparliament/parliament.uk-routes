72722cec6ed173b42eb25f2ae951b92629590961
Callum Neve-Jones <c.nevejones@gmail.com>
2017-09-11 08:54:36 +0100

    Added 'people' to 'people/committees#index'.
    * Removed lookup that we don't need yet.

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'routing' do
   it 'should have a specific number of routes inherited from the engine' do
-    # routes.rb currently has 150 routes, 2 routes exist on top of this in dummy application
-    expect(Rails.application.routes.routes.size).to eq(152)
+    # routes.rb currently has 149 routes, 2 routes exist on top of this in dummy application
+    expect(Rails.application.routes.routes.size).to eq(151)
   end
 end
