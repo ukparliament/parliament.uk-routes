97e569902e9c33f3e867693f05ee5c0278c32c34
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2018-02-22 14:31:22 +0000

    Add collections#show route
    * Add route
    * Update tests
    * Update version number to 0.5.5

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'routing' do
   it 'should have a specific number of routes inherited from the engine' do
-    # routes.rb currently has 158 routes, 2 routes exist on top of this in dummy application
-    expect(Rails.application.routes.routes.size).to eq(160)
+    # routes.rb currently has 160 routes, 2 routes exist on top of this in dummy application
+    expect(Rails.application.routes.routes.size).to eq(162)
   end
 end
