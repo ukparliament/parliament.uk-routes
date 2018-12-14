8943f0dbbc363b2acf959b4045b932c631a03623
Christine-horrocks <splogy2001@hotmail.com>
2018-12-14 17:09:59 +0000

    SI lookup

--- lib/parliament/engine/version.rb
+++ lib/parliament/engine/version.rb
@@ -1,5 +1,5 @@
 module Parliament
   module Engine
-    VERSION = '0.6.29'.freeze
+    VERSION = '0.6.30'.freeze
   end
 end
--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe 'routing' do
   it 'should have a specific number of routes inherited from the engine' do
     # routes.rb currently has 219 routes, 2 routes exist on top of this in dummy application
-    expect(Rails.application.routes.routes.size).to eq(221)
+    expect(Rails.application.routes.routes.size).to eq(222)
   end
 end
